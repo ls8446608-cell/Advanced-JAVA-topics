3fea351e2ca2ab2674704caab7e25ebe54a96604
Lokesh Saini <ls8446608@gmail.com>
2026-08-23 23:13:36 +0530

    todays

--- Advance.java
+++ Advance.java
@@ -91,6 +91,121 @@ public class Advance {
 
     }
 
+
+    public static void towerOfHanoi(int n, String source , String helper , String dest){
+        if(n==1){
+     System.out.println("Transfer "+n+ " from "+source+ " to "+ dest);
+            return ;
+
+        }
+        towerOfHanoi(n-1, source, dest, helper);
+        System.out.println("Transfer "+n+ " from "+source+ " to "+ dest);
+        towerOfHanoi(n-1, helper, source, dest);
+
+    }
+
+    public static void printRev(String str , int idx){
+        if(idx==0){
+            System.out.println(str.charAt(idx));
+            return;
+        }
+        System.out.println(str.charAt(idx));
+        printRev(str, idx-1);
+    }
+
+
+    public static int first =-1;
+    public static int last =-1;
+
+    public static void findOccuerence(String str , int idx , char ele){
+        
+        if(idx==str.length()){
+            System.out.println(first);
+            System.out.println(last);
+            return;
+        }
+        char currentChar = str.charAt(idx);
+        if(currentChar==ele){
+            if(first==-1){
+                first = idx;
+            }else{
+                last =idx;
+            }
+            
+        }
+        
+        if(first == -1 && last== -1){
+            System.out.println("Character does not belongs to this string");
+            return;
+
+        }
+        
+        findOccuerence(str, idx+1, ele);
+
+
+    }
+
+
+    public static boolean  findSorted(int [] arr , int idx){
+        if(idx==arr.length -1){
+            return true;
+
+        }
+        if(arr[idx]>=arr[idx+1]){
+            return false;
+        }
+         return findSorted(arr, idx+1);
+        // if(arr[idx]<arr[idx+1]){
+            
+        //     return findSorted(arr, idx+1);
+            
+        // }else{
+        //     return false;
+        // }
+    
+    }
+
+
+    
+    public static void moveToLast(String str, int idx , int count , char ele ,String newStr){
+        if(idx==str.length()-1){
+            for(int i=0; i<=count; i++){
+                newStr+=ele;
+            }
+            System.out.println(newStr);
+            return;
+        }
+        
+        if(str.charAt(idx)!= ele){
+            newStr+=str.charAt(idx);
+            
+        }else{
+            count++;
+            
+        }
+        moveToLast(str, idx+1, count, ele,newStr);
+    }
+
+
+    public static boolean[] map = new boolean[26];
+
+    public static void removeDupe(String str , int idx, String newString){
+        if(idx==str.length()){
+            System.out.println(newString);
+            return;
+        }
+
+        char current = str.charAt(idx);
+        if(map[current-'a']== true){
+            removeDupe(str, idx+1, newString);
+
+        }else{
+            newString+=current;
+            map[current-'a']=true;
+            removeDupe(str, idx+1, newString);
+        }
+    }
+
     public static void main(String[] args) {
 
         //-------------------Bubbole Sort-------------------//
@@ -161,8 +276,34 @@ public class Advance {
         // System.out.println(b);
         // toPrintFibo(n,a,b);
 
-      int ans =  printXPowerN(2, 5);
-      System.out.println(ans);
+    //   int ans =  printXPowerN(2, 5);
+    //   System.out.println(ans);
+
+
+    // int n =4;
+    // String S ="S";
+    // String H = "H";
+    // String D = "D";
+    // towerOfHanoi(n, S, H, D);
+
+    // String name = "lokesh";
+    // printRev(name, name.length()-1);
+
+    // String word = "abbcdasdadcdsa";
+    
+    
+    // findOccuerence(word, 0, 'a');
+
+    // int[] arr = {1,2,3,6,5};
+    // System.out.println(findSorted(arr, 0));
+
+    // String str ="abxcsdfdgddfgbxcdxd";
+    // String newStr =" ";
+    // moveToLast(str, 0, 0, 'c', newStr);
+
+
+    String str ="abbcdefghiiiessikwwwlaaad";
+    removeDupe(str, 0, " ");
     }
 
 }
